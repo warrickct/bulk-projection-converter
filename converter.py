5a44a2e1f5204b32945a7a75f4f25fd0f2e33a8d
Warrick Corfe-Tan <warrick@Warricks-MacBook-Pro.local>
2018-06-25 17:29:45 +1200

    First commit, added example output file

---
+++ converter.py
@@ -0,0 +1,35 @@
+import csv
+from pyproj import Proj, transform
+
+def convert_coordinate(x, y):
+    '''
+    Casts coordinates into numeric floats, converts them from NZGD2000 to epsg:4326
+    '''
+    x = float(x)
+    y = float(y)
+    inProj = Proj(init='epsg:2193')
+    outProj = Proj(init='epsg:4326')
+    out_x, out_y = transform(inProj, outProj, x, y)
+    return out_x, out_y
+
+# Change to file you want to convert.
+file = open('./Gavin_water_data_2010_metadata.tsv')
+reader = csv.reader(file, delimiter='\t')
+# Change change to desired output name
+with open("./new_meta.tsv", "w") as output_file:
+    firstLine = True
+    header_row = []
+    for row in reader:
+        if firstLine:
+            firstLine = False
+            header_row = row
+            output_file.write('\t'.join(row) + '\n')
+            continue
+        # Change to match the column numbers that need to be converted.
+        x, y = convert_coordinate(row[1], row[2])
+        converted_row = row
+        converted_row[1] = str(x)
+        converted_row[2] = str(y)
+        output_file.write("\t".join(converted_row) + '\n')
+
+
