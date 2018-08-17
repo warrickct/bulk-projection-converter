dff31443faff0b802c33bf9ca1ee57e14b16e24d
warrick <wcor690@aucklanduni.ac.nz>
2018-08-17 12:32:13 +1200

    added additional option to just output the rows to be converted online as pyproj doesnt support NZTM2000

--- converter.py
+++ converter.py
@@ -5,6 +5,15 @@ def convert_coordinate(x, y):
     '''
     Casts coordinates into numeric floats, converts them from NZGD2000 to epsg:4326
     '''
+    coordinate_system = ({
+        # New Zealand Mercator 2000
+        "NZGD2000": "epsg:2193",
+        # New Zealand Transverse Mercator 2000. Not supported by pyproj
+        "NZTM2000": "epsg:19971",
+        # WGS 84
+        "WGS84": "epsg:4326",
+        })
+    
     x = x.replace('--', '-')
     y = y.replace('--', '-')
     x = float(x)
@@ -14,11 +23,19 @@ def convert_coordinate(x, y):
     out_x, out_y = transform(inProj, outProj, x, y)
     return out_x, out_y
 
+def output_only_rows(x, y, writer):
+    x = x.replace('-', '')
+    y = y.replace('-', '')
+    print(y, x)
+    writer.writerow([y, x])
+
 # Change to file you want to convert.
 file = open('./input/Syrie-metadata-trimmed.csv')
 reader = csv.reader(file, delimiter=',')
 # Change change to desired output name
+output_rows = input('output just the rows? [y/n]: ')
 with open("./output/new_meta.tsv", "w") as output_file:
+    writer = csv.writer(output_file, delimiter=" ")
     firstLine = True
     header_row = []
     for row in reader:
@@ -28,10 +45,13 @@ with open("./output/new_meta.tsv", "w") as output_file:
             output_file.write('\t'.join(row) + '\n')
             continue
         # Change to match the column numbers that need to be converted.
-        x, y = convert_coordinate(row[1], row[2])
-        converted_row = row
-        converted_row[1] = str(x)
-        converted_row[2] = str(y)
-        output_file.write("\t".join(converted_row) + '\n')
+        if output_rows == "y" or output_rows == "Y":
+            output_only_rows(row[1], row[2], writer)
+        else:
+            x, y = convert_coordinate(row[1], row[2])
+            converted_row = row
+            converted_row[1] = str(x)
+            converted_row[2] = str(y)
+            output_file.write("\t".join(converted_row) + '\n')
 
 
